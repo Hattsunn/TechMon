adec31984adbbf0fc7cadaccf5cdbc7224ed44eb
okada hatsune <swan.nilda1211@gmail.com>
2020-05-13 14:44:42 +0900

    Initial Commit

---
+++ TechMon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TechMon
+//
+//  Created by Hatsune Okada on 5/13/20.
+//  Copyright © 2020 Okada Hattsunn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
